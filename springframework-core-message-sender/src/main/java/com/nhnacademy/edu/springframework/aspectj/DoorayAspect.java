439f4c1606ae28c567c2e05fc359f6b8c1b120ef
halo-eunsu <ujd017@naver.com>
2024-02-04 22:54:30 +0900

    1

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/aspectj/DoorayAspect.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/aspectj/DoorayAspect.java
@@ -1,5 +1,8 @@
 package com.nhnacademy.edu.springframework.aspectj;
 
+import com.nhnacademy.edu.springframework.DoorayMessageSender;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,8 +11,11 @@ import org.springframework.util.StopWatch;
 
 
 @Aspect
+
 public class DoorayAspect {
 
+
+    private static final Log log = LogFactory.getLog(DoorayAspect.class);
 
     @Pointcut("execution(* com.nhnacademy.edu.springframework.*.sendMessage(..)) " +
             "&& @annotation(com.nhnacademy.edu.springframework.TimeLogging))")
@@ -29,7 +35,8 @@ public class DoorayAspect {
         Object result = joinPoint.proceed();
 
         stopWatch.stop();
-        System.out.printf("%s.%s %dms%n", joinPoint.getTarget().getClass().getSimpleName(), methodName, stopWatch.getTotalTimeMillis());
+        //System.out.printf("%s.%s %dms%n", joinPoint.getTarget().getClass().getSimpleName(), methodName, stopWatch.getTotalTimeMillis());
+        log.info(String.format("%s.%s %dms", joinPoint.getTarget().getClass().getSimpleName(), methodName, stopWatch.getTotalTimeMillis()));
 
 
         return result;
