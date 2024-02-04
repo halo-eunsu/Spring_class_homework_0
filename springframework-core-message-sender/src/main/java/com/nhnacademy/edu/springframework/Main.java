189daf22055feac786723839900a2e61133f2532
halo-eunsu <ujd017@naver.com>
2024-02-04 21:49:28 +0900

    1

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -5,26 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-
 
         User user = new User("정은수", "");
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
             MessageSendService messageSendService = (MessageSendService) context.getBean("sendService");
 
-
             messageSendService.doSendMessage(user, "안녕하세요");
 
-
-
-
-
-
     }
-
-
-
-
-
 }
 }
