1e6731fabf2298de074fbad9ad573c8a6d6e5c06
halo-eunsu <ujd017@naver.com>
2024-02-05 07:43:19 +0900

    1

--- springframework-core-message-sender/src/test/java/com/nhnacademy/edu/springframework/DoorayMessageSenderTest.java
+++ springframework-core-message-sender/src/test/java/com/nhnacademy/edu/springframework/DoorayMessageSenderTest.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.*;
 public class DoorayMessageSenderTest {
 
     @Test
-    public void testSendMessage() {
+    public void sendMessageTest() {
 
         String userName = "User";
         String message = "Message";
